72d30010523409a08c9040002f64a09125c57aa8
LeonidKrutovsky <leonid.krutovsky@quantumsoft.ru>
2020-08-18 00:08:50 +0700

    Add message parser

---
+++ WST/wsdl11/src/xml_to_wsdl/message.rs
@@ -0,0 +1,28 @@
+use crate::model::{Message, Documentation};
+use roxmltree::Node;
+use xsd10::xml_to_xsd::ElementChildren;
+use crate::xml_to_wsdl::WsdlNode;
+use crate::model::elements::ElementType;
+use crate::model::complex_types::t_part::Part;
+
+impl<'a> Message<'a> {
+    pub fn parse(node: Node<'a, '_>) -> Result<Self, String> {
+        let mut res = Self::default();
+
+        for attr in node.attributes() {
+            match attr.name() {
+                "name" => res.name = attr.into(),
+                _ => return Err(format!("Invalid attribute. {:?}", node)),
+            }
+        }
+
+        for ch in node.element_children() {
+            match ch.wsdl_type()? {
+                ElementType::Documentation => res.documentation = Some(Documentation::parse(ch)?),
+                ElementType::Part => res.part = Part::parse(ch)?,
+                _ => res.elements.push(ch),
+            }
+        }
+        Ok(res)
+    }
+}
