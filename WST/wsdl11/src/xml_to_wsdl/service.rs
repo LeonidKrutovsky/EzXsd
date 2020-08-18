cae6c70fa85233a0755d3af2ae0e776c7be25599
LeonidKrutovsky <leonid.krutovsky@quantumsoft.ru>
2020-08-18 21:42:43 +0700

    Add service parser

---
+++ WST/wsdl11/src/xml_to_wsdl/service.rs
@@ -0,0 +1,34 @@
+use crate::model::{Service, Port, Documentation};
+use roxmltree::Node;
+use crate::model::elements::ElementType;
+use crate::xml_to_wsdl::WsdlNode;
+use xsd10::xml_to_xsd::ElementChildren;
+use xsd10::model::simple_types::NCName;
+
+impl<'a> Service<'a> {
+    pub fn parse(node: Node<'a, '_>) -> Result<Self, String> {
+        let mut res = Self::default();
+        let mut name = None;
+
+        for attr in node.attributes() {
+            match attr.name() {
+                "name" => name = Some(NCName::from(attr)),
+                _ => return Err(format!("Invalid attribute. {:?}", node)),
+            }
+        }
+
+        res.name = name
+            .ok_or_else(|| format!("Name attribute required: {:?}", node))?
+            .into();
+
+        for ch in node.element_children() {
+            match ch.wsdl_type() {
+                Ok(ElementType::Documentation) => res.documentation = Some(Documentation::parse(ch)?),
+                Ok(ElementType::Port) => res.ports.push(Port::parse(ch)?),
+                _ => res.elements.push(ch),
+            }
+        }
+
+        Ok(res)
+    }
+}
