16e22d0a06987762807cd8e9226e95f378b0d689
Leonid Krutovsky <leonid.krutovsky@quantumsoft.ru>
2021-02-28 12:40:45 +0700

    Add anySimpleType

---
+++ CXT/xsd10/src/model/simple_types/any_simple_type.rs
@@ -0,0 +1,11 @@
+// xsd:anySimpleType
+// The type xsd:anySimpleType is the base type from which all other built-in types are derived. Any value (including an empty value) is allowed for xsd:anySimpleType.
+//
+// Simple Type Information
+// Namespace: http://www.w3.org/2001/XMLSchema
+//
+// Schema Document: datatypes.xsd
+
+use std::borrow::Cow;
+
+pub type AnySimpleType<'a> = Cow<'a, str>;
