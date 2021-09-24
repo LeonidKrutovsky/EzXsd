6d880453431de8bbda2f02960ef712f18762b10d
Leonid Krutovsky <leonid.krutovsky@quantumsoft.ru>
2021-09-25 00:25:56 +0700

    add parse and NAMES to redefinable

--- CXT/xsd10/src/model/groups/redefinable.rs
+++ CXT/xsd10/src/model/groups/redefinable.rs
@@ -2,7 +2,6 @@ use crate::model::elements::attribute_group::AttributeGroup;
 use crate::model::elements::complex_type::TopLevelComplexType;
 use crate::model::elements::group::Group;
 use crate::model::elements::simple_type::TopLevelSimpleType;
-use roxmltree::Node;
 
 // xsd:redefinable
 // This group is for the elements which can self-redefine.
@@ -36,14 +35,26 @@ impl Default for Redefinable {
 }
 
 impl Redefinable {
+    pub const NAMES: &'static [&'static str] = &[
+        TopLevelSimpleType::NAME,
+        TopLevelComplexType::NAME,
+        Group::NAME,
+        AttributeGroup::NAME,
+    ];
+
     pub fn parse(node: roxmltree::Node<'_, '_>) -> Result<Self, String> {
         match node.tag_name().name() {
-            TopLevelSimpleType::NAME => Ok(Self::SimpleType(Box::new(TopLevelSimpleType::parse(node)?))),
-            TopLevelComplexType::NAME => Ok(Self::ComplexType(Box::new(TopLevelComplexType::parse(node)?))),
+            TopLevelSimpleType::NAME => {
+                Ok(Self::SimpleType(Box::new(TopLevelSimpleType::parse(node)?)))
+            }
+            TopLevelComplexType::NAME => Ok(Self::ComplexType(Box::new(
+                TopLevelComplexType::parse(node)?,
+            ))),
             Group::NAME => Ok(Self::Group(Box::new(Group::parse(node)?))),
-            AttributeGroup::NAME => Ok(Self::AttributeGroup(Box::new(AttributeGroup::parse(node)?))),
-            _ => Err(String::default())
+            AttributeGroup::NAME => {
+                Ok(Self::AttributeGroup(Box::new(AttributeGroup::parse(node)?)))
+            }
+            _ => Err(String::default()),
         }
-
     }
 }
