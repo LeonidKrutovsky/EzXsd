82d6fad87ffeff835a309c993d3a9d8870103f79
Leonid Krutovsky <leonid.krutovsky@quantumsoft.ru>
2021-09-27 21:53:42 +0700

    add parse to ElementModel

--- CXT/xsd10/src/model/groups/element_model.rs
+++ CXT/xsd10/src/model/groups/element_model.rs
@@ -30,6 +30,33 @@ pub struct ElementModel {
 
 #[derive(Debug)]
 pub enum ElementModelChoice {
-    SimpleType(Box<LocalSimpleType>),
-    ComplexType(Box<LocalComplexType>),
+    SimpleType(LocalSimpleType),
+    ComplexType(LocalComplexType),
+}
+
+impl ElementModel {
+    pub fn parse(node: &roxmltree::Node) -> Result<Self, String> {
+        let mut choice: Option<ElementModelChoice> = None;
+        let mut identity_constraints: Vec<IdentityConstraint> = vec![];
+        for ch in node.children().filter(|n| n.is_element()) {
+            match ch.tag_name().name() {
+                LocalSimpleType::NAME => {
+                    choice = Some(ElementModelChoice::SimpleType(LocalSimpleType::parse(ch)?))
+                }
+                LocalComplexType::NAME => {
+                    choice = Some(ElementModelChoice::ComplexType(LocalComplexType::parse(
+                        ch,
+                    )?))
+                }
+                tag_name if IdentityConstraint::NAMES.contains(&tag_name) => {
+                    identity_constraints.push(IdentityConstraint::parse(ch)?)
+                }
+                _ => {}
+            }
+        }
+        Ok(Self {
+            choice,
+            identity_constraints,
+        })
+    }
 }
