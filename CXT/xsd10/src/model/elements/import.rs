04f1271c75478ea621c1488dd6e26e46833668d1
Leonid Krutovsky <leonid.krutovsky@quantumsoft.ru>
2021-10-04 21:54:20 +0700

    remove import.rs

--- CXT/xsd10/src/model/elements/import.rs
+++ CXT/xsd10/src/model/elements/import.rs
@@ -33,3 +33,28 @@ pub struct Import {
     pub namespace: Option<attributes::NamespaceUri>,
     pub schema_location: Option<attributes::SchemaLocation>,
 }
+
+#[cfg(test)]
+mod test {
+    use super::Import;
+    #[test]
+    fn test_parse() {
+        let doc = roxmltree::Document::parse(
+            r#"<import id="ID" namespace="xsd" schemaLocation="http://uri" b='a'>
+                    <annotation>
+                        <appinfo>Some appinfo</appinfo>
+                        <documentation>Some doc2</documentation>
+                    </annotation>
+            </import>"#,
+        )
+        .unwrap();
+        let root = doc.root_element();
+        let res: Import = Import::parse(root).unwrap();
+        assert_eq!(res.annotation.as_ref().unwrap().documentations.len(), 1);
+        assert_eq!(res.annotation.unwrap().app_infos.len(), 1);
+        assert_eq!(res.attributes.len(), 1);
+        assert_eq!(res.id.unwrap().0.as_ref(), "ID");
+        assert_eq!(res.schema_location.unwrap().0.as_ref(), "http://uri");
+        assert_eq!(res.namespace.unwrap().0.as_ref(), "xsd");
+    }
+}
