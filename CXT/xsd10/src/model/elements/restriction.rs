f28f7f161a6a385fcf1d9fd91fe1bfb72424561d
Leonid Krutovsky <leonid.krutovsky@quantumsoft.ru>
2021-10-04 22:24:33 +0700

    remove complex_restriction.rs

--- CXT/xsd10/src/model/elements/restriction.rs
+++ CXT/xsd10/src/model/elements/restriction.rs
@@ -87,6 +87,9 @@ pub struct ComplexRestriction {
 #[cfg(test)]
 mod test {
     use super::*;
+    use crate::model::groups::TypeDefParticle;
+    use crate::model::attributes::ProcessContents;
+
     #[test]
     fn test_parse_restriction() {
         let doc = roxmltree::Document::parse(
@@ -113,7 +116,7 @@ mod test {
         )
         .unwrap();
         let root = doc.root_element();
-        let res = Restriction::parse(root).unwrap();
+        let res:Restriction = Restriction::parse(root).unwrap();
         assert!(res.annotation.is_some());
         assert_eq!(res.attributes.len(), 2);
         assert_eq!(res.id.unwrap().0.as_ref(), "ID");
@@ -122,4 +125,76 @@ mod test {
         let model = &res.model;
         assert_eq!(model.facets.len(), 12);
     }
+
+     #[test]
+    fn test_empty() {
+        let doc = roxmltree::Document::parse(
+            r#"<xs:restriction base="tns:BarType" xmlns:xs="http://www.w3.org/2001/XMLSchema" a='a' b='b' id="ID">
+                    </xs:restriction>"#,
+        )
+        .unwrap();
+        let root = doc.root_element();
+        let res:ComplexRestriction = ComplexRestriction::parse(root).unwrap();
+        assert!(res.annotation.is_none());
+        assert_eq!(res.base.0.name(), "BarType");
+        assert_eq!(res.base.0.prefix(), Some("tns"));
+        assert!(res.type_def_particle.is_none());
+        assert_eq!(res.attributes.len(), 2);
+        assert_eq!(res.id.unwrap().0.as_ref(), "ID");
+        assert_eq!(res.attr_decls.attributes.len(), 0);
+    }
+
+    #[test]
+    fn test_full() {
+        let doc = roxmltree::Document::parse(
+            r###"<xs:restriction base="tns:BarType" xmlns:xs="http://www.w3.org/2001/XMLSchema">
+                        <xs:annotation>
+							<xs:documentation>Text</xs:documentation>
+						</xs:annotation>
+                        <xs:sequence>
+                            <xs:annotation>
+                                <xs:documentation>Text</xs:documentation>
+                            </xs:annotation>
+                        </xs:sequence>
+                        <xs:attribute name="Attr1" type="xs:unsignedInt" use="required"/>
+                        <xs:attribute name="Attr2" type="xs:anyURI"/>
+                        <xs:attribute name="Attr3" type="xs:unsignedInt" use="required"/>
+                        <xs:anyAttribute namespace="##other" processContents="lax"/>
+                    </xs:restriction>"###,
+        )
+        .unwrap();
+        let root = doc.root_element();
+        let res:ComplexRestriction = ComplexRestriction::parse(root).unwrap();
+        assert!(res.annotation.is_some());
+        if let TypeDefParticle::Sequence(val) = res.type_def_particle.unwrap() {
+            assert!(val.annotation.is_some());
+        } else {
+            panic!()
+        }
+
+        let attr = &res.attr_decls;
+        assert_eq!(attr.attributes.len(), 3);
+        assert_eq!(attr.any_attribute.as_ref().unwrap().process_contents, ProcessContents::Lax);
+    }
+
+    #[test]
+    fn test_only_attr_decls() {
+        let doc = roxmltree::Document::parse(
+            r###"<xs:restriction base="tns:BarType" xmlns:xs="http://www.w3.org/2001/XMLSchema">
+                        <xs:attribute name="Attr1" type="xs:unsignedInt" use="required"/>
+                        <xs:attribute name="Attr2" type="xs:anyURI"/>
+                        <xs:attribute name="Attr3" type="xs:unsignedInt" use="required"/>
+                        <xs:anyAttribute namespace="##other" processContents="lax"/>
+                    </xs:restriction>"###,
+        )
+        .unwrap();
+        let root = doc.root_element();
+        let res:ComplexRestriction = ComplexRestriction::parse(root).unwrap();
+        assert!(res.annotation.is_none());
+        assert!(res.type_def_particle.is_none());
+        let attr = &res.attr_decls;
+        assert_eq!(attr.attributes.len(), 3);
+        assert_eq!(attr.any_attribute.as_ref().unwrap().process_contents, ProcessContents::Lax);
+    }
 }
+
